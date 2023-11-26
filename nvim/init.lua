f80d20b0d49cdab776a353478b56448c246016e3
Jōshin <git@wholezero.org>
2023-11-25 21:32:43 -0500

    initial

---
+++ nvim/init.lua
@@ -0,0 +1,53 @@
+vim.cmd('syntax off')
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.list = true
+vim.opt.showbreak = '↪ '
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.mouse = ''
+vim.g.mapleader = ';'
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require'lazy'.setup {
+  { 'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function ()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "bash",
+          "beancount",
+          "c",
+          "cpp",
+          "css",
+          "git_rebase",
+          "gitcommit",
+          "go",
+          "hoon",
+          "html",
+          "javascript",
+          "lua",
+          "python",
+          "rust",
+          "tsx",
+          "typescript",
+          "vim",
+        },
+        indent = { enable = true },
+      }
+    end
+  },
+}
