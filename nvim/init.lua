56a64a9af17762e96e4c5b2256544d7d9b897bdc
Jōshin <git@wholezero.org>
2023-11-27 01:32:29 -0500

    nvim: nvim-cmp and lsp-zero

--- nvim/init.lua
+++ nvim/init.lua
@@ -82,4 +82,73 @@ require'lazy'.setup {
       { '<leader>u', vim.cmd.UndotreeToggle },
     },
   },
+
+  { 'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = { },
+  },
+
+  { 'williamboman/mason.nvim',
+    config = true,
+  },
+
+  { 'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function ()
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
+
+  { 'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      { 'L3MON4D3/LuaSnip' },
+    },
+    config = function ()
+      local lsp_zero = require'lsp-zero'
+      lsp_zero.extend_cmp()
+      local cmp = require'cmp'
+      local cmp_action = lsp_zero.cmp_action()
+      cmp.setup {
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert {
+          ['<CR>'] = cmp.mapping.confirm { select = false },
+          -- ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        }
+      }
+    end,
+  },
+
+  { 'neovim/nvim-lspconfig',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function ()
+      local lsp_zero = require'lsp-zero'
+      lsp_zero.extend_lspconfig()
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps { buffer = bufnr }
+      end)
+      require'mason-lspconfig'.setup {
+        ensure_installed = {},
+        handlers = {
+          lsp_zero.default_setup,
+        },
+      }
+    end,
+    dependencies = {
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'williamboman/mason-lspconfig.nvim' },
+    },
+  },
 }
