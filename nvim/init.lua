258dddd2ce798a81d4957c2c0d65614dc3d9596c
Jōshin <git@wholezero.org>
2023-11-27 02:56:01 -0500

    nvim: supertab, no autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,6 +117,11 @@ require'lazy'.setup {
       local cmp = require'cmp'
       local cmp_action = lsp_zero.cmp_action()
       cmp.setup {
+        preselect = 'item',
+        completion = {
+          autocomplete = false,
+          completeopt = 'menu,menuone,noinsert',
+        },
         formatting = lsp_zero.cmp_format(),
         mapping = cmp.mapping.preset.insert {
           ['<CR>'] = cmp.mapping.confirm { select = false },
@@ -125,6 +130,8 @@ require'lazy'.setup {
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
           ['<C-f>'] = cmp_action.luasnip_jump_forward(),
           ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ['<Tab>'] = cmp_action.luasnip_supertab(),
+          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
         }
       }
     end,
