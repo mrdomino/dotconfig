a5315d5343ef21ca96c07fcffe325c8d6d5de1b7
Jōshin <git@wholezero.org>
2023-11-26 20:12:02 -0500

    nvim: treesitter auto_install

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,6 +34,7 @@ require'lazy'.setup {
     build = ':TSUpdate',
     config = function ()
       require'nvim-treesitter.configs'.setup {
+        auto_install = true,
         ensure_installed = {
           'bash',
           'beancount',
