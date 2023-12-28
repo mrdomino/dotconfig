a075b5313c790d2bc6a7d6b7789f6fd385293856
Jōshin <git@wholezero.org>
2023-12-28 12:50:23 -0500

    nvim: LuaSnip, c-space

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,6 +117,7 @@ require'lazy'.setup {
       local lsp_zero = require'lsp-zero'
       lsp_zero.extend_cmp()
       local cmp = require'cmp'
+      local cmp_action = lsp_zero.cmp_action()
       cmp.setup {
         preselect = 'item',
         completion = {
@@ -126,12 +127,17 @@ require'lazy'.setup {
         formatting = lsp_zero.cmp_format(),
         mapping = cmp.mapping.preset.insert {
           ['<CR>'] = cmp.mapping.confirm { select = false },
-          -- ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
         }
       }
     end,
+    dependencies = {
+      { 'L3MON4D3/LuaSnip' },
+    },
   },
 
   { 'neovim/nvim-lspconfig',
