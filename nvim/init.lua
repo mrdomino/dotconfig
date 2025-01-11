a854791be51f9ced5cc7d045bffd1191c50df5dc
Steven Dee (Jōshin) <mrdomino@stairwell.com>
2025-01-11 12:34:02 -0800

    nvim: update telescope to 0.1.8

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,7 +70,7 @@ require'lazy'.setup {
     },
     lazy = false,
   },
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.4',
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x',
     cmd = 'Telescope',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
