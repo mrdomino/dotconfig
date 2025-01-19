c2f55db067944cc6750d694c93592f1a34dfcd95
Steven Dee (Jōshin) <git@wholezero.org>
2025-01-18 22:14:46 -0800

    nix-darwin follows

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
