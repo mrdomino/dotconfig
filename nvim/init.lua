ba51907c0ec582863defa86ff01e832ae0223608
Steven Dee (Jōshin) <git@wholezero.org>
2025-06-16 17:41:02 -0700

    nvim: update, new parrot config?

--- nvim/init.lua
+++ nvim/init.lua
@@ -285,7 +285,45 @@ require('lazy').setup {
       require('parrot').setup {
         providers = {
           anthropic = {
+            name = 'anthropic',
+            endpoint = 'https://api.anthropic.com/v1/messages',
+            model_endpoint = 'https://api.anthropic.com/v1/models',
             api_key = os.getenv 'ANTHROPIC_API_KEY',
+            params = {
+              chat = { max_tokens = 4096 },
+              command = { max_tokens = 4096 },
+            },
+            topic = {
+              model = 'claude-3-5-haiku-latest',
+              params = { max_tokens = 32 },
+            },
+            headers = function(self)
+              return {
+                ['Content-Type'] = 'application/json',
+                ['x-api-key'] = self.api_key,
+                ['anthropic-version'] = '2023-06-01',
+              }
+            end,
+            models = {
+              'claude-sonnet-4-20250514',
+              'claude-3-7-sonnet-20250219',
+              'claude-3-5-sonnet-20241022',
+              'claude-3-5-haiku-20241022',
+            },
+            preprocess_payload = function(payload)
+              for _, message in ipairs(payload.messages) do
+                message.content = message.content:gsub('^%s*(.-)%s*$', '%1')
+              end
+              if
+                payload.messages[1] and payload.messages[1].role == 'system'
+              then
+                -- remove the first message that serves as the system prompt as anthropic
+                -- expects the system prompt to be part of the API call body and not the messages
+                payload.system = payload.messages[1].content
+                table.remove(payload.messages, 1)
+              end
+              return payload
+            end,
           },
         },
       }
