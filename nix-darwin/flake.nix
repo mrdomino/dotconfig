3276d6b7fc8bd0e2438f94f1cc191d1eab6989d3
Steven Dee <git@wholezero.org>
2025-07-08 23:42:03 -0700

    nix-darwin: update, unpin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,16 +2,13 @@
   description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    inputs@{
+    {
       self,
       nix-darwin,
-      nixpkgs,
     }:
     let
       configuration =
